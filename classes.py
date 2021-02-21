6086d7636b74bfe63214bbd8ad4faba0e05a931a
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-02-21 16:07:24 +0300

    parameters tab

--- classes.py
+++ classes.py
@@ -19,6 +19,62 @@ class main_window():
         self.tab_control.add(self.tab_controller, text='Controller')
         self.tab_control.add(self.tab_parameters, text='Parameters')
         self.tab_control.pack(expand=1, fill='both')
-        
+
+        # Parameters tab init UI
+        #sample time
+        self.lbl_sample_time = tk.Label(self.tab_parameters, text='Sample time')
+        self.lbl_sample_time.grid(row=0, column=0, padx=10)
+        self.ent_sample_time = tk.Entry(self.tab_parameters)
+        self.ent_sample_time.grid(row=0, column=1)
+        # Controller sample time
+        self.lbl_cont_sample_time = tk.Label(self.tab_parameters, text='Controller sample time')
+        self.lbl_cont_sample_time.grid(row=1, column=0, padx=10)
+        self.ent_cont_sample_time = tk.Entry(self.tab_parameters)
+        self.ent_cont_sample_time.grid(row=1, column=1)
+        # Initial conditions
+        self.lbl_init_cond = tk.Label(self.tab_parameters, text='Initial conditions')
+        self.lbl_init_cond.grid(row=2, column=0, padx=10)
+        self.ent_init_cond = tk.Entry(self.tab_parameters)
+        self.ent_init_cond.grid(row=2, column=1)
+        # Input delay
+        self.lbl_input_delay = tk.Label(self.tab_parameters, text='Input delay')
+        self.lbl_input_delay.grid(row=3, column=0, padx=10)
+        self.ent_input_delay = tk.Entry(self.tab_parameters)
+        self.ent_input_delay.grid(row=3, column=1)
+        self.status_input_delay = tk.BooleanVar()
+        self.status_input_delay.set(False)
+        self.chb_input_delay = tk.Checkbutton(self.tab_parameters, var=self.status_input_delay)
+        self.chb_input_delay.grid(row=3, column=2, padx=10)
+        # Output delay
+        self.lbl_output_delay = tk.Label(self.tab_parameters, text='Output delay')
+        self.lbl_output_delay.grid(row=4, column=0, padx=10)
+        self.ent_output_delay = tk.Entry(self.tab_parameters)
+        self.ent_output_delay.grid(row=4, column=1)
+        self.status_output_delay = tk.BooleanVar()
+        self.status_output_delay.set(False)
+        self.chb_output_delay = tk.Checkbutton(self.tab_parameters, var=self.status_output_delay)
+        self.chb_output_delay.grid(row=4, column=2, padx=10)
+        # Input quantizing
+        self.lbl_input_quant = tk.Label(self.tab_parameters, text='Input quantizing')
+        self.lbl_input_quant.grid(row=5, column=0, padx=10)
+        self.ent_input_quant = tk.Entry(self.tab_parameters)
+        self.ent_input_quant.grid(row=5, column=1)
+        self.status_input_quant = tk.BooleanVar()
+        self.status_input_quant.set(False)
+        self.chb_input_quant = tk.Checkbutton(self.tab_parameters, var=self.status_input_quant)
+        self.chb_input_quant.grid(row=5, column=2, padx=10)
+        # Output quantizing
+        self.lbl_output_quant = tk.Label(self.tab_parameters, text='Output quantizing')
+        self.lbl_output_quant.grid(row=6, column=0, padx=10)
+        self.ent_output_quant = tk.Entry(self.tab_parameters)
+        self.ent_output_quant.grid(row=6, column=1)
+        self.status_output_quant = tk.BooleanVar()
+        self.status_output_quant.set(False)
+        self.chb_output_quant = tk.Checkbutton(self.tab_parameters, var=self.status_output_quant)
+        self.chb_output_quant.grid(row=6, column=2, padx=10)
+        # Save button
+        self.btn_save_parameters = tk.Button(self.tab_parameters, text='Save')
+        self.btn_save_parameters.grid(row=7, column=0, columnspan=3, sticky='we', padx=10, pady=10)
+
 
 
