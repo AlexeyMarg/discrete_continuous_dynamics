4350ee46f7c7b312db608f591bd23d7cec59b4b1
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-03-06 18:38:16 +0300

    canvas on model tab

--- classes.py
+++ classes.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import  scrolledtext
+from tkinter import scrolledtext
 
 class main_window():
     def __init__(self, app):
         self.app = app
         self.app.title('Continuous plant with discrete controller')
-        self.app.geometry('400x500')
+        self.app.geometry('700x500')
         self.init_ui()
 
     def init_ui(self):
@@ -14,10 +14,12 @@ class main_window():
         self.tab_model = ttk.Frame(self.tab_control)
         self.tab_plant = ttk.Frame(self.tab_control)
         self.tab_controller = ttk.Frame(self.tab_control)
+        self.tab_input = ttk.Frame(self.tab_control)
         self.tab_parameters = ttk.Frame(self.tab_control)
         self.tab_control.add(self.tab_model, text='Modeling')
         self.tab_control.add(self.tab_plant, text='Plant')
         self.tab_control.add(self.tab_controller, text='Controller')
+        self.tab_control.add(self.tab_input, text='Input')
         self.tab_control.add(self.tab_parameters, text='Parameters')
         self.tab_control.pack(expand=1, fill='both')
 
@@ -93,3 +95,32 @@ class main_window():
         self.btn_reset_plant = tk.Button(self.tab_plant, text='Reset')
         self.btn_reset_plant.grid(row=2, column=0, sticky='we', padx=10, pady=10)
 
+        #Input tab
+        self.txt_input = scrolledtext.ScrolledText(self.tab_input, width=45, height=20)
+        self.txt_input.grid(row=0, column=0, sticky='we', padx=10, pady=10)
+        self.btn_save_input = tk.Button(self.tab_input, text='Save')
+        self.btn_save_input.grid(row=1, column=0, sticky='we', padx=10)
+        self.btn_reset_input = tk.Button(self.tab_input, text='Reset')
+        self.btn_reset_input.grid(row=2, column=0, sticky='we', padx=10, pady=10)
+
+        #Modeling tab
+        self.model_cns = tk.Canvas(self.tab_model, width=600, height=400, bg='white')
+        self.model_cns.grid(row=0, column=0)
+        self.model_cns.create_line(10, 50, 70, 50, width=2, arrow='last', arrowshape='5 10 5')
+        self.model_cns.create_oval(70, 40, 90, 60)
+        self.model_cns.create_line(90, 50, 110, 50, width=2, arrow='last', arrowshape='5 10 5')
+        self.model_cns.create_rectangle(110, 30, 180, 70)
+        self.model_cns.create_text(145, 50, text='Controller')
+        self.model_cns.create_line(180, 50, 350, 50, width=2, arrow='last', arrowshape='5 10 5')
+        self.model_cns.create_rectangle(350, 30, 420, 70)
+        self.model_cns.create_text(385, 50, text='Plant')
+        self.model_cns.create_line(420, 50, 480, 50, width=2, arrow='last', arrowshape='5 10 5')
+        self.model_cns.create_line(450, 50, 450, 130, width=2)
+        self.model_cns.create_line(450, 130, 80, 130, width=2)
+        self.model_cns.create_line(80, 130, 80, 60, width=2, arrow='last', arrowshape='5 10 5')
+        self.model_cns.create_text(80, 55, text='-')
+        self.model_cns.create_text(75, 50, text='+')
+        self.model_cns.create_text(40, 35, text='g(t)')
+        self.model_cns.create_text(100, 35, text='e(t)')
+        self.model_cns.create_text(195, 35, text='u(t)')
+        self.model_cns.create_text(435, 35, text='y(t)')
